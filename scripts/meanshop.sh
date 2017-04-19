5b63c192a0908eb1ef5766923474dee0980d12cb
Josh Cox <josh@webhosting.coop>
2017-04-19 01:54:59 -0500

    slackurl

--- scripts/meanshop.sh
+++ scripts/meanshop.sh
@@ -2,3 +2,17 @@
 echo Running dummy script
 echo $1
 echo $2
+
+if [ ! -z ${DEBUG+x} ]
+  then
+    echo ENV
+    print-env
+fi
+
+if [ ! -z ${SLACK_URL+x} ]
+  then
+    echo "Slack URL detected sending slack"
+    curl -X POST --data-urlencode \
+    'payload={"channel": "#github", "username": "webhookbot", "text": "Captain Hook has detected that the meanshop build has finished.", "icon_emoji": ":ghost:"}' \
+    $SLACK_URL
+fi
