af6db648d84b8a47fd6e83919e169def2f650080
Josh Cox <josh@webhosting.coop>
2017-04-19 00:58:54 -0500

    meanshop

--- scripts/index.js
+++ scripts/index.js
@@ -3,5 +3,6 @@
 module.exports = {
   'maccyber/testhook': 'hello.sh parameter1 parameter2', // namespace/repo_name : script <parameters>
   'maccyber/maccyber.io': 'maccyber.io.sh',
+  'thalhalla/meanshop': 'meanshop.sh parameter1 parameter2', // namespace/repo_name : script <parameters>
   'fail': 'fail.sh'
 }
