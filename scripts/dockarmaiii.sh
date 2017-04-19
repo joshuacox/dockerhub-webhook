d77d508a9c847d072b7af736b49e6ca2c683c5e6
Josh Cox <josh@webhosting.coop>
2017-04-19 10:14:26 -0500

    script

---
+++ scripts/dockarmaiii.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+echo $1
+echo $2
+
+if [ ! -z ${DEBUG+x} ]
+  then
+    echo ENV
+    print-env
+fi
+
+if [ ! -z ${SLACK_URL+x} ]
+  then
+    echo "Slack URL detected sending slack"
+    curl -X POST --data-urlencode \
+    'payload={"channel": "#github", "username": "CaptainHook", "text": "Captain Hook has detected that the dockarmaiii container has hit the open seas.", "icon_emoji": ":whale:"}' \
+    $SLACK_URL
+fi
---
+++ scripts/dontstarvewithfriends.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+echo Running Dont Starve with Friends script
+echo $1
+echo $2
+
+if [ ! -z ${DEBUG+x} ]
+  then
+    echo ENV
+    print-env
+fi
+
+if [ ! -z ${SLACK_URL+x} ]
+  then
+    echo "Slack URL detected sending slack"
+    curl -X POST --data-urlencode \
+    'payload={"channel": "#github", "username": "CaptainHook", "text": "Captain Hook has detected that the dontstarvewithfriends container has hit the open seas.", "icon_emoji": ":whale:"}' \
+    $SLACK_URL
+fi
