55d4f6642665c383fa5ef2d5f740991b650d4008
Josh Cox <josh@webhosting.coop>
2017-06-17 12:48:44 -0500

    bokker

--- scripts/multiurl.sh
+++ scripts/multiurl.sh
@@ -12,9 +12,7 @@ fi
 if [ ! -z ${SLACK_URL+x} ]
   then
     echo "Slack URL detected sending slack"
-    curl -X POST --data-urlencode \
-    "payload={\"channel\": \"#github\", \"username\": \"CaptainHook\", \"text\": \"Captain Hook has detected that the ${1} container has hit the open seas.\", \"icon_emoji\": \":whale:\"}" \
-    $SLACK_URL
+    ./bokker $SLACK_URL
 fi
 
 if [ ! -z ${MATTERMOST_URL+x} ]
