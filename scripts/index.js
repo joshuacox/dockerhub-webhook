865af957209c0b2eb04f8dd58545f908a7fe96a1
Josh Cox <josh@webhosting.coop>
2017-06-17 14:28:43 -0500

    repos

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,26 @@
 'use strict'
 
 module.exports = {
+  'bokbot/dotorg': 'multiurl.sh dotorg roboks CaptainHook',
+  'bokbot/bokbotapp': 'multiurl.sh bokbotapp roboks CaptainHook',
+  'gaianysus/meanshop': 'multiurl.sh Gaiashop roboks CaptainHook',
+  'joshuacox/dockerhub-webhook': 'multiurl.sh captainhook roboks CaptainHook',
+  'joshuacox/mkdomoticz': 'multiurl.sh mkdomoticz roboks CaptainHook',
+  'joshuacox/mktunnel': 'multiurl.sh mktunnel roboks CaptainHook',
+  'joshuacox/octossh': 'multiurl.sh octossh roboks CaptainHook',
+  'joshuacox/mktrojanhorse': 'multiurl.sh mktrojanhorse roboks CaptainHook',
+  'joshuacox/replaceme': 'multiurl.sh replaceme roboks CaptainHook',
   'thalhalla/meanshop': 'multiurl.sh Meanshop roboks CaptainHook',
   'thalhalla/steamer': 'multiurl.sh steamer roboks CaptainHooksteamer.sh',
   'thalhalla/dockarmaiii': 'multiurl.sh DockARMAiii roboks CaptainHook',
   'thalhalla/tf2': 'multiurl.sh TF2 roboks CaptainHook',
   'thalhalla/7daystodie': 'multiurl.sh 7daystodie roboks CaptainHook',
   'thalhalla/dontstarvewithfriends': 'multiurl.sh dontstarvewithfriends.sh roboks CaptainHook',
-  'joshuacox/dockerhub-webhook': 'multiurl.sh captainhook roboks CaptainHook',
-  'bokbot/dotorg': 'multiurl.sh dotorg roboks CaptainHook',
-  'bokbot/bokbotapp': 'multiurl.sh bokbotapp roboks CaptainHook',
   'webhostingcoopteam/snowplow-scala-collector': 'multiurl.sh snowplow-scala-collector  roboks CaptainHook',
-  'gaianysus/meanshop': 'multiurl.sh Gaiashop roboks CaptainHook',
+  'webhostingcoopteam/nginx-tiny-proxy': 'multiurl.sh nginx-tiny-proxy roboks CaptainHook',
+  'webhostingcoopteam/wwiv': 'multiurl.sh wwiv roboks CaptainHook',
+  'webhostingcoopteam/openemr': 'multiurl.sh openemr roboks CaptainHook',
+  'webhostingcoopteam/rubynpm': 'multiurl.sh rubynpm roboks CaptainHook',
+  'replaceme': 'multiurl.sh replaceme roboks CaptainHook',
   'fail': 'fail.sh'
 }
