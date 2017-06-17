4aa91304158c3a2ff82dc2b1610c4f601aa20b24
Josh Cox <josh@webhosting.coop>
2017-06-17 13:24:04 -0500

    bokker

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,16 @@
 'use strict'
 
 module.exports = {
-  'maccyber/testhook': 'hello.sh parameter1 parameter2', // namespace/repo_name : script <parameters>
-  'maccyber/maccyber.io': 'maccyber.io.sh',
   'thalhalla/meanshop': 'meanshop.sh parameter1 parameter2', // namespace/repo_name : script <parameters>
   'thalhalla/steamer': 'steamer.sh parameter1 parameter2',
   'thalhalla/dockarmaiii': 'dockarmaiii.sh parameter1 parameter2',
   'thalhalla/tf2': 'tf2.sh parameter1 parameter2',
   'thalhalla/7daystodie': '7daystodie.sh parameter1 parameter2',
   'thalhalla/dontstarvewithfriends': 'dontstarvewithfriends.sh parameter1 parameter2',
-  'joshuacox/dockerhub-webhook': 'multiurl.sh captainhook #roboks CaptainHook',
-  'bokbot/dotorg': 'multiurl.sh dotorg dockerhub',
-  'bokbot/bokbotapp': 'multiurl.sh bokbotapp dockerhub',
-  'webhostingcoopteam/snowplow-scala-collector': 'multiurl.sh snowplow-scala-collector dockerhub',
-  'gaianysus/meanshop': 'multiurl.sh gaiashop dockerhub',
+  'joshuacox/dockerhub-webhook': 'multiurl.sh captainhook roboks CaptainHook',
+  'bokbot/dotorg': 'multiurl.sh dotorg roboks CaptainHook',
+  'bokbot/bokbotapp': 'multiurl.sh bokbotapp roboks CaptainHook',
+  'webhostingcoopteam/snowplow-scala-collector': 'multiurl.sh snowplow-scala-collector  roboks CaptainHook',
+  'gaianysus/meanshop': 'multiurl.sh gaiashop roboks CaptainHook',
   'fail': 'fail.sh'
 }
